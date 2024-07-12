b62f71dfb833b8b500283ff3a85add8d9124d1ba
UlisesChacon <uliseschacon96@gmail.com>
2024-07-11 19:00:30 -0500

    Primer Push - Proyecto grupo 4 - Ulises - menu

--- src/utp/proyecto/vista/InicioVista.java
+++ src/utp/proyecto/vista/InicioVista.java
@@ -33,11 +33,11 @@ public class InicioVista extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 990, Short.MAX_VALUE)
+            .addGap(0, 1000, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 580, Short.MAX_VALUE)
+            .addGap(0, 590, Short.MAX_VALUE)
         );
 
         pack();
